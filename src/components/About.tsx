d38a56129923ee08f0ce221044043eae8ddde9be
Andrew213455 <woodring.andrew@yahoo.com>
2023-09-26 17:34:39 -0400

    about page mostly complete, started on portfolio page (scroll bar, pictures, styling, pop up)

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,9 @@
 import "./About.css";
 import cloud from "../images/wordcloudthiiisone.png";
 import skills from "../images/MERN skills.jpg";
+import circus from "../images/grand_circus.webp";
+import background from "../images/me-bartending.jpg";
+import future from "../images/future.jpg";
 
 const About = () => {
   return (
@@ -21,11 +24,11 @@ const About = () => {
                 growth, and I am confident that I can make a significant
                 contribution to any team.
               </p>
-              <img src={cloud} alt="" />
+              <img src={cloud} alt="Word Cloud" />
             </div>
 
             <div className="skills">
-              <img src={skills} alt="" />
+              <img src={skills} alt="MERN Stack Picture" />
               <p>
                 I started out as a hobby programmer with a strong foundation in{" "}
                 <span className="blue-letters">
@@ -39,9 +42,9 @@ const About = () => {
               </p>
             </div>
             <div className="education">
-              <h3>Education</h3>
+              <img src={circus} alt="Grand Circus Programming Bootcamp" />
               <p>
-                I achieved an overall score of 99% in my JavaScript Full Stack
+                I achieved an overall score of 99% in the JavaScript Full Stack
                 bootcamp at Grand Circus in 2023. I have worked on exciting
                 projects such as "Zombie Village" and "Culinary Caddy", where I
                 was able to contribute to the development of the project
@@ -49,7 +52,7 @@ const About = () => {
               </p>
             </div>
             <div className="background">
-              <h3>Background</h3>
+              <img src={background} alt="me bartending" />
               <p>
                 I have a strong background in customer service, as a trainer and
                 bartender at Texas RoadHouse from 2021 to 2023. This has given
@@ -59,7 +62,7 @@ const About = () => {
               </p>
             </div>
             <div className="future">
-              <h3>Future</h3>
+              <img src={future} alt="generic picture of the future" />
               <p>
                 I am excited about the abundant opportunities for growth and
                 skill development in the software engineering field, and I am
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,7 +1,77 @@
 import "./Portfolio.css";
+import CC from "../images/iconcc.jpg";
+import Village from "../images/zombieVillage.png";
+import Penguin from "../images/club-penguin.png";
+import Vibe from "../images/Vibe-link.png";
+import { useState } from "react";
 
 const Portfolio = () => {
-  return <div className="Portfolio">Portfolio works</div>;
+  const [popUp, setPopUp] = useState(false);
+  const [project, setProject] = useState("");
+
+  return (
+    <div className="Portfolio">
+      <div className="project-container">
+        <div className="project-box">
+          <p className="blue-letters">Culinary Caddy</p>
+          <img
+            src={CC}
+            alt="Culinary Caddy"
+            onClick={() => {
+              setPopUp(true);
+              setProject("caddy");
+            }}
+          />
+        </div>
+        <div className="project-box">
+          <p className="blue-letters">Zombie Village</p>
+          <img
+            src={Village}
+            alt="Zombie Village Game"
+            onClick={() => {
+              setPopUp(true);
+              setProject("zombie");
+            }}
+          />
+        </div>
+        <div className="project-box">
+          <p className="blue-letters">Club Penguin Budgeting</p>
+          <img
+            src={Penguin}
+            alt="Club Penguin Budgeting app"
+            onClick={() => {
+              setPopUp(true);
+              setProject("penguin");
+            }}
+          />
+        </div>
+        <div className="project-box">
+          <p className="blue-letters">Vibe Link</p>
+          <img
+            src={Vibe}
+            alt="Vibe-Link"
+            onClick={() => {
+              setPopUp(true);
+              setProject("vibe");
+            }}
+          />
+        </div>
+      </div>
+
+      {popUp && (
+        <div className="pop-up">
+          <button
+            onClick={() => {
+              setPopUp(false);
+            }}
+          >
+            X
+          </button>
+        </div>
+      )}
+      {popUp && <div className="black"></div>}
+    </div>
+  );
 };
 
 export default Portfolio;
